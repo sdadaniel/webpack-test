782ca0b0f8e45bf83db3684d1423afd3a13831c0
sdadaniel0206 <sdadaniel0206@gmail.com>
2023-03-14 11:27:55 +0900

    add sass weback config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./source/index.js",
+  entry: { src1: "./source1/index.js", src2: "./source2/index.js" },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
@@ -13,6 +14,16 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
+  // plugins: [
+  //   // extract css to external stylesheet file
+  //   new MiniCssExtractPlugin({
+  //     filename: "build/styles.css",
+  //   }),
+  // ],
 };
